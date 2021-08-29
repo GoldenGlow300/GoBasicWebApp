d834f79588e0c77756839d1326bafce1d24bdcd7
Malesa Williams <Malesaw900@gmail.com>
2021-08-29 16:41:11 -0500

    implementation of the template cache

--- cmd/pkg/render/render.go
+++ cmd/pkg/render/render.go
@@ -1,17 +1,74 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"net/http"
+	"path/filepath"
 	"text/template"
 )
 
-//This renders the templates that were created, this func is called in the corressponding page functions
+var functions = template.FuncMap{}
+
+//This renders the templates that were created, this func is called in the corresponding page functions
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
+
+	tc, err := CreateTemplateCache()
 	if err != nil {
-		fmt.Println("Error parsing template: ", err)
-		return
+		log.Fatal(err)
 	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal(err)
+	}
+
+	buf := new(bytes.Buffer)
+
+	_ = t.Execute(buf, nil)
+
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		fmt.Println("Error writing template to browser", err)
+	}
+}
+
+//CreateTemplateCache creates a template cache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	//this is where the templates will be cached
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	//where page is the full path to the file, but we need the name of the file only in this case
+	for _, page := range pages {
+
+		//.Base gets the file name
+		name := filepath.Base(page)
+
+		templateSet, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			templateSet, err = templateSet.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = templateSet
+	}
+
+	return myCache, nil
 }
